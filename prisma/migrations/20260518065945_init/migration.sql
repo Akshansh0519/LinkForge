9af3708996cbb0c980d7a0bbc357aed22baf1235
Akshansh Ranjan <akshanshranjan0519@gmail.com>
2026-05-23 00:25:57 +0530

    chore: commit initial database migration

---
+++ prisma/migrations/20260518065945_init/migration.sql
@@ -0,0 +1,56 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Url" (
+    "id" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "original" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Url_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Click" (
+    "id" TEXT NOT NULL,
+    "urlId" TEXT NOT NULL,
+    "country" TEXT,
+    "device" TEXT,
+    "referrer" TEXT,
+    "clickedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Click_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Url_slug_key" ON "Url"("slug");
+
+-- CreateIndex
+CREATE INDEX "Url_slug_idx" ON "Url"("slug");
+
+-- CreateIndex
+CREATE INDEX "Url_userId_createdAt_idx" ON "Url"("userId", "createdAt");
+
+-- CreateIndex
+CREATE INDEX "Click_urlId_clickedAt_idx" ON "Click"("urlId", "clickedAt");
+
+-- AddForeignKey
+ALTER TABLE "Url" ADD CONSTRAINT "Url_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Click" ADD CONSTRAINT "Click_urlId_fkey" FOREIGN KEY ("urlId") REFERENCES "Url"("id") ON DELETE CASCADE ON UPDATE CASCADE;
